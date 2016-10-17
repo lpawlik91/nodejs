ff8d8632269bd1dee0852c16a0e67960847b7d3a
lpawlik91 <lukasz.pawlik@91.pl>
2016-10-17 23:46:18 +0200

    simple adding new book

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var bodyParser = require('body-parser');
 
 app.get('/listBooks', function (req, res) {
    fs.readFile( __dirname + "/" + "books.json", 'utf8', function (err, data) {
@@ -8,6 +9,52 @@ app.get('/listBooks', function (req, res) {
       res.end( data );
    });
 })
+
+
+
+
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/add_book.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "add_book.html" );
+})
+
+app.post('/addBook', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+ //  var test = req.body.title;
+	 toAdd = { 
+      title:req.body.title,
+      author:req.body.author,
+	  cover:req.body.cover
+	
+		};
+      var dataToSave;
+  fs.readFile(__dirname + "/" + "books.json" , 'utf8', function (err, data) {
+	  
+
+      data = JSON.parse( data );
+      data[req.body.title] = toAdd;
+	  dataToSave = data;
+      console.log( data );
+	  
+	fs.writeFile(__dirname + "/" + "books.json", JSON.stringify(data), function(err) {
+		if(err) {
+			return console.log(err);
+		}
+
+		console.log("The file was saved!");
+		res.end( JSON.stringify(data));
+	}); 
+      
+   });
+   
+   
+
+})
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
