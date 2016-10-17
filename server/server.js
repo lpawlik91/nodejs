56a626a055e6bea2bf5162ba8876e6311b65daaf
lpawlik91 <lukasz.pawlik@91.pl>
2016-10-18 00:45:06 +0200

    quick refactor

--- server/server.js
+++ server/server.js
@@ -3,57 +3,55 @@ var app = express();
 var fs = require("fs");
 var bodyParser = require('body-parser');
 
-app.get('/listBooks', function (req, res) {
-   fs.readFile( __dirname + "/" + "books.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
-})
 
-
-
-
-
-// Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.use(express.static('public'));
-app.get('/add_book.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "add_book.html" );
-})
-
-app.post('/addBook', urlencodedParser, function (req, res) {
-   // Prepare output in JSON format
- //  var test = req.body.title;
-	 toAdd = { 
-      title:req.body.title,
-      author:req.body.author,
-	  cover:req.body.cover
+var booksPath = __dirname + "/" + "books.json";
 	
+var Action = {
+	
+	getListBooks : function (req, res) {
+	   fs.readFile( __dirname + "/" + "books.json", 'utf8', function (err, data) {
+		  console.log( data );
+		  res.end( data );
+	   });
+	},
+	getAddingBookForm : function(req, res) {
+		res.sendFile( __dirname + "/" + "add_book.html" );
+	},	
+	postAddBook : function (req, res) {
+		var toAdd = { 
+			title:req.body.title,
+			author:req.body.author,
+			cover:req.body.cover
+
 		};
-      var dataToSave;
-  fs.readFile(__dirname + "/" + "books.json" , 'utf8', function (err, data) {
-	  
+		Action.readBooksFromFile(req, res, toAdd);
+	},
+	readBooksFromFile : function (req, res, toAdd) {
+		fs.readFile( booksPath, 'utf8', function (err, data) {
+			data = JSON.parse( data );
+			data[req.body.title] = toAdd;
+			console.log( data );
+			Action.dumpNewBooksFile(res, data);
 
-      data = JSON.parse( data );
-      data[req.body.title] = toAdd;
-	  dataToSave = data;
-      console.log( data );
-	  
-	fs.writeFile(__dirname + "/" + "books.json", JSON.stringify(data), function(err) {
-		if(err) {
-			return console.log(err);
-		}
+		});
+	},
+	dumpNewBooksFile : function (res, data) {
+		fs.writeFile(booksPath, JSON.stringify(data), function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("The file was saved!");
+			res.end( JSON.stringify(data));
+		}); 
+	},
+	
+	
+};
 
-		console.log("The file was saved!");
-		res.end( JSON.stringify(data));
-	}); 
-      
-   });
-   
-   
-
-})
+app.get('/listBooks', Action.getListBooks);
+app.get('/add_book.html', Action.getAddingBookForm);
+app.post('/addBook', urlencodedParser, Action.postAddBook)
 
 
 var server = app.listen(8081, function () {
